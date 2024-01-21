349a78cd0e4b6c0d8e8bae6ebb7f50a38e6819fb
Fredrik Strömberg <fredrikstromberg90@gmail.com>
2024-01-21 19:01:19 +0100

    handle errors and loading

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,47 @@ import Introduction from './components/Introduction'
 import CoffeeCard from './components/CoffeeCard'
 
 const App = () => {
-  const { data: coffees } = useCoffees()
+  const { data: coffees, isError, error, isLoading } = useCoffees()
   const [activeButton, setActiveButton] = useState<string>('')
+  const [filtered, setFiltered] = useState(false)
+
+  const filteredData = coffees?.filter((coffee) => coffee.available)
 
   return (
     <div className='main'>
       <Introduction />
       <div className='btn-container'>
         <Button
-          onClick={() => setActiveButton('allProducts')}
+          onClick={() => {
+            setActiveButton('allProducts')
+            setFiltered(false)
+          }}
           isActive={activeButton === 'allProducts'}
+          disabled={!coffees}
         >
           All Products
         </Button>
         <Button
-          onClick={() => setActiveButton('availableNow')}
+          onClick={() => {
+            setActiveButton('availableNow')
+            setFiltered(true)
+          }}
           isActive={activeButton === 'availableNow'}
+          disabled={!coffees}
         >
           Available Now
         </Button>
       </div>
+      {isLoading && <p className='loading'>Loading...</p>}
+      {isError && <p className='error'>{error.message}</p>}
       <div className='coffee-list'>
-        {coffees?.map((coffee) => (
-          <CoffeeCard key={coffee.id} {...coffee} />
-        ))}
+        {filtered
+          ? filteredData?.map((coffee) => (
+              <CoffeeCard key={coffee.id} {...coffee} />
+            ))
+          : coffees?.map((coffee) => (
+              <CoffeeCard key={coffee.id} {...coffee} />
+            ))}
       </div>
     </div>
   )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,14 @@ type Props = {
   children: string
   onClick: () => void
   isActive: boolean
+  disabled: boolean
 }
 
-const Button = ({ children, onClick, isActive }: Props) => {
+const Button = ({ children, onClick, isActive, disabled }: Props) => {
   return (
     <button
       className={`btn  ${isActive ? 'btn-active' : ''}`}
+      disabled={disabled}
       onClick={onClick}
     >
       {children}
